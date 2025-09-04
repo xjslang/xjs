eccd22bcad975a7d643c3dd47ef6f415fdd51116
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-09-04 23:26:33 +0100

    Add missing carriage return test case and update output

--- test/testdata/escape_sequences.js
+++ test/testdata/escape_sequences.js
@@ -4,4 +4,5 @@ console.log('String with \'escaped single quotes\'');
 console.log("Backslash: \\");
 console.log("Tab:\tafter tab");
 console.log("Newline:\nafter newline");
+console.log("Carriage return:\rbefore carriage return");
 console.log("Mixed: \"Hello\", she said. It's a \\\"test\\\".");
