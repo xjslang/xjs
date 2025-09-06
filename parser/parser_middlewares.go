5a1a5263302f105f8f3819dd66d590675dad6ee7
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-09-06 12:00:18 +0100

    Add TODO comments to rename UseStatementHandler and UseExpressionHandler in parser_middlewares.go

--- parser/parser_middlewares.go
+++ parser/parser_middlewares.go
@@ -4,6 +4,7 @@ import (
 	"github.com/xjslang/xjs/ast"
 )
 
+// TODO: rename to UsseStatementParser(parser...) and update the README.md file
 func (p *Parser) UseStatementHandler(handler func(p *Parser, next func() ast.Statement) ast.Statement) {
 	next := p.statementParseFn
 	p.statementParseFn = func(p *Parser) ast.Statement {
@@ -13,6 +14,7 @@ func (p *Parser) UseStatementHandler(handler func(p *Parser, next func() ast.Sta
 	}
 }
 
+// TODO: rename to UseExpressionParser(parser...) and update the README.md file
 func (p *Parser) UseExpressionHandler(handler func(p *Parser, next func() ast.Expression) ast.Expression) {
 	next := p.expressionParseFn
 	p.expressionParseFn = func(p *Parser, precedence int) ast.Expression {
