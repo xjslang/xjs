66a17aa7a80c673d71b123fa8402d48f315e3aab
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-09-09 22:34:06 +0100

    Refactor RegisterInfixOperator and RegisterPrefixOperator for improved expression parsing

--- parser/parser_functions.go
+++ parser/parser_functions.go
@@ -189,17 +189,31 @@ func (p *Parser) ParseInfixExpression(left ast.Expression) ast.Expression {
 	return infix(left)
 }
 
-func (p *Parser) RegisterInfixOperator(operatorToken string, precedence int, createExpr func(token token.Token, left, right ast.Expression) ast.Expression) {
+func (p *Parser) RegisterPrefixOperator(literal string, createExpr func(right func() ast.Expression) ast.Expression) {
 	p.UseExpressionParser(func(p *Parser, next func() ast.Expression) ast.Expression {
-		if p.PeekToken.Literal != operatorToken {
+		if p.CurrentToken.Literal == literal {
+			right := func() ast.Expression {
+				p.NextToken()
+				return p.ParseExpression()
+			}
+			return createExpr(right)
+		}
+		return next()
+	})
+}
+
+func (p *Parser) RegisterInfixOperator(literal string, precedence int, createExpr func(left ast.Expression, right func() ast.Expression) ast.Expression) {
+	p.UseExpressionParser(func(p *Parser, next func() ast.Expression) ast.Expression {
+		if p.PeekToken.Literal != literal {
 			return next()
 		}
 		left := p.ParsePrefixExpression()
 		p.NextToken() // consume operator
-		token := p.CurrentToken
-		p.NextToken() // move to right expression
-		right := p.ParseExpressionWithPrecedence(precedence)
-		expr := createExpr(token, left, right)
+		right := func() ast.Expression {
+			p.NextToken() // move to right expression
+			return p.ParseExpressionWithPrecedence(precedence)
+		}
+		expr := createExpr(left, right)
 		return p.ParseRemainingExpression(expr)
 	})
 }
