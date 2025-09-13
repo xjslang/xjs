f6f2c4acc52b1504708c0b3f612a2f9e25a735f2
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-09-13 18:17:04 +0100

    Refactor Dump function to accept any type instead of ast.Node for improved flexibility

--- internal/debug.go
+++ internal/debug.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"github.com/davecgh/go-spew/spew"
-	"github.com/xjslang/xjs/ast"
 )
 
 var cfg = &spew.ConfigState{
@@ -13,6 +12,6 @@ var cfg = &spew.ConfigState{
 }
 
 // Dump prints the AST node structure for debugging
-func Dump(node ast.Node) {
+func Dump(node any) {
 	cfg.Dump(node)
 }
