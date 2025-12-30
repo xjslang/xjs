775fd7ec9693037cbc0a3e88985cb99847927841
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-12-30 21:42:17 +0000

    Remove unused ParserErrors type and related methods from errors.go

--- parser/errors.go
+++ parser/errors.go
@@ -1,10 +1,5 @@
 package parser
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 type Position struct {
 	Line   int `json:"line"`
 	Column int `json:"column"`
@@ -15,24 +10,4 @@ type ParserError struct {
 	Position Position `json:"position"`
 	Length   int      `json:"length"`
 	Code     string   `json:"code,omitempty"`
-}
-
-type ParserErrors struct {
-	Errors []ParserError `json:"errors"`
-	Source string        `json:"source,omitempty"`
-}
-
-func (pe ParserErrors) Error() string {
-	if len(pe.Errors) == 1 {
-		return fmt.Sprintf("parse error at line %d, column %d: %s",
-			pe.Errors[0].Position.Line,
-			pe.Errors[0].Position.Column,
-			pe.Errors[0].Message)
-	}
-	return fmt.Sprintf("parsing failed with %d errors (first: %s)",
-		len(pe.Errors), pe.Errors[0].Message)
-}
-
-func (pe ParserErrors) ToJSON() ([]byte, error) {
-	return json.Marshal(pe)
 }
