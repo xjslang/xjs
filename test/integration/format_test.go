3e7462596fa6a05696994ae888bb1a7da890c0ec
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-01-03 23:23:03 +0000

    Add integration test for pretty-print functionality

---
+++ test/integration/format_test.go
@@ -0,0 +1,69 @@
+package integration
+
+import (
+	"fmt"
+
+	"github.com/xjslang/xjs/compiler"
+	"github.com/xjslang/xjs/lexer"
+	"github.com/xjslang/xjs/parser"
+)
+
+func Example_format() {
+	input := `
+		// init vars
+		let x = 5
+		let y = 10.5
+		let name = "Hello World"
+
+		// push items
+		let items = []
+		items.push(function () {
+			console.log("new item")
+		})
+
+		// add function
+		function add(a, b) {
+			return a + b
+		}
+
+		// conditional
+		if (x < y) {
+			console.log("x is less than y")
+		}
+
+		// init vars
+		let numbers = [1, 2, 3, 4, 5]
+		let person = {name: "John", age: 30}
+	`
+	lb := lexer.NewBuilder()
+	p := parser.NewBuilder(lb).Build(input)
+	program, err := p.ParseProgram()
+	if err != nil {
+		fmt.Printf("ParseProgram error = %v\n", err)
+		return
+	}
+	result := compiler.New().WithPrettyPrint().Compile(program)
+	fmt.Println(result.Code)
+	// Output:
+	// // init vars
+	// let x = 5;
+	// let y = 10.5;
+	// let name = "Hello World";
+	// // push items
+	// let items = [];
+	// items.push(function() {
+	//   console.log("new item");
+	// }
+	// );
+	// // add function
+	// function add(a, b) {
+	//   return (a + b);
+	// }
+	// // conditional
+	// if ((x < y)) {
+	//   console.log("x is less than y");
+	// }
+	// // init vars
+	// let numbers = [1, 2, 3, 4, 5];
+	// let person = {age: 30, name: "John"};
+}
