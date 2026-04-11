f5455c3ebb01f7cfc3eb230a20dac163da335c9e
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-04-11 21:38:19 +0100

    feat: add debug.Print function for testing only

---
+++ debug/debug.go
@@ -0,0 +1,14 @@
+package debug
+
+import "github.com/davecgh/go-spew/spew"
+
+var cfg = &spew.ConfigState{
+	Indent:                  "   ",
+	DisableMethods:          true,
+	DisablePointerAddresses: true,
+	ContinueOnMethod:        false,
+}
+
+func Print(node ...any) {
+	cfg.Dump(node)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/xjslang/xjs
 
 go 1.24
 
-require github.com/magefile/mage v1.17.1
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/magefile/mage v1.17.1
+)
