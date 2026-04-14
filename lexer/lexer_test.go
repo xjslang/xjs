dae02700b9fa0b7ebc9574ced8132483360b2eb3
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-04-14 22:21:28 +0100

    refactor(testing): remove expectTokenSequence() and perform custom comparisons instead

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,16 +7,49 @@ import (
 	"github.com/xjslang/xjs/token"
 )
 
-func expectTokenSequence(t *testing.T, input string, expectedToks []token.Token) {
-	l := New(strings.NewReader(input))
+func TestAfterNewline(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+	}{
+		{"newline before block comment", "hello\n/* block comment */world"},
+		{"block comment with newline", "hello/* block\ncomment */world"},
+		{"newline", "hello\nworld"},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			expectedToks := []token.Token{
+				{Type: token.IDENT, Literal: "hello"},
+				{Type: token.IDENT, Literal: "world", AfterNewline: true},
+			}
+			l := New(strings.NewReader(test.input))
+			for i, expectedTok := range expectedToks {
+				tok := l.NextToken()
+				if tok.Type != expectedTok.Type {
+					t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+				} else if tok.Literal != expectedTok.Literal {
+					t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+				} else if tok.AfterNewline != expectedTok.AfterNewline {
+					t.Errorf("token %d: expected AfterNewline to be %t, got %t", i, expectedTok.AfterNewline, tok.AfterNewline)
+				}
+			}
+		})
+	}
+}
+
+func TestEmptySinglelineComment(t *testing.T) {
+	expectedToks := []token.Token{
+		{Type: token.IDENT, Literal: "hello", LeadingTrivia: []string{""}},
+		{Type: token.IDENT, Literal: "there", LeadingTrivia: []string{""}},
+		{Type: token.EOF, LeadingTrivia: []string{"\rObi-Wan Kenobi"}},
+	}
+	l := New(strings.NewReader("//\nhello//\r\nthere//\rObi-Wan Kenobi"))
 	for i, expectedTok := range expectedToks {
 		tok := l.NextToken()
 		if tok.Type != expectedTok.Type {
 			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
 		} else if tok.Literal != expectedTok.Literal {
 			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
-		} else if tok.AfterNewline != expectedTok.AfterNewline {
-			t.Errorf("token %d: expected AfterNewline to be %t, got %t", i, expectedTok.AfterNewline, tok.AfterNewline)
 		} else if expectedTok.LeadingTrivia != nil && len(tok.LeadingTrivia) != len(expectedTok.LeadingTrivia) {
 			t.Errorf("token %d: expected %d leading trivia lines, got %d", i, len(expectedTok.LeadingTrivia), len(tok.LeadingTrivia))
 		} else {
@@ -29,70 +62,86 @@ func expectTokenSequence(t *testing.T, input string, expectedToks []token.Token)
 	}
 }
 
-func TestAfterNewline(t *testing.T) {
-	tests := []struct {
-		name  string
-		input string
-	}{
-		{"newline before block comment", "hello\n/* block comment */world"},
-		{"block comment with newline", "hello/* block\ncomment */world"},
-		{"newline", "hello\nworld"},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			expectTokenSequence(t, test.input, []token.Token{
-				{Type: token.IDENT, Literal: "hello"},
-				{Type: token.IDENT, Literal: "world", AfterNewline: true},
-			})
-		})
-	}
-}
-
-func TestEmptySinglelineComment(t *testing.T) {
-	expectTokenSequence(t, "//\nhello//\r\nthere//\rObi-Wan Kenobi", []token.Token{
-		{Type: token.IDENT, Literal: "hello", LeadingTrivia: []string{""}, AfterNewline: true},
-		{Type: token.IDENT, Literal: "there", LeadingTrivia: []string{""}, AfterNewline: true},
-		{Type: token.EOF, LeadingTrivia: []string{"\rObi-Wan Kenobi"}, AfterNewline: true},
-	})
-}
-
 func TestMultilineComments(t *testing.T) {
-	expectTokenSequence(t, `/* lorem
+	expectedToks := []token.Token{
+		{Type: token.IDENT, Literal: "hello", LeadingTrivia: []string{" lorem\nipsum dolor ", "", ""}},
+		{Type: token.ILLEGAL, Literal: " unfinished comment"},
+		{Type: token.EOF},
+	}
+	l := New(strings.NewReader(`/* lorem
 ipsum dolor */
 
-hello/* unfinished comment`, []token.Token{
-		{Type: token.IDENT, Literal: "hello", LeadingTrivia: []string{" lorem\nipsum dolor ", "", ""}, AfterNewline: true},
-		{Type: token.ILLEGAL, Literal: " unfinished comment", AfterNewline: false},
-		{Type: token.EOF},
-	})
+hello/* unfinished comment`))
+	for i, expectedTok := range expectedToks {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+		} else if tok.Literal != expectedTok.Literal {
+			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+		} else if expectedTok.LeadingTrivia != nil && len(tok.LeadingTrivia) != len(expectedTok.LeadingTrivia) {
+			t.Errorf("token %d: expected %d leading trivia lines, got %d", i, len(expectedTok.LeadingTrivia), len(tok.LeadingTrivia))
+		} else {
+			for j, line := range expectedTok.LeadingTrivia {
+				if tok.LeadingTrivia[j] != line {
+					t.Errorf("token %d: expected %q leading trivia line, got %q", i, line, tok.LeadingTrivia[j])
+				}
+			}
+		}
+	}
 }
 
 func TestSinglelineComments(t *testing.T) {
-	expectTokenSequence(t, `
+	expectedToks := []token.Token{
+		{Type: token.IDENT, Literal: "John", LeadingTrivia: []string{"", " First Name"}},
+		{Type: token.IDENT, Literal: "Smith", LeadingTrivia: []string{"", "", " Last Name"}},
+		{Type: token.EOF, LeadingTrivia: []string{"", "", " Final comment"}},
+	}
+	l := New(strings.NewReader(`
   // First Name
   John
   
   // Last Name
   Smith
 	
-	// Final comment`, []token.Token{
-		{Type: token.IDENT, Literal: "John", LeadingTrivia: []string{"", " First Name"}, AfterNewline: true},
-		{Type: token.IDENT, Literal: "Smith", LeadingTrivia: []string{"", "", " Last Name"}, AfterNewline: true},
-		{Type: token.EOF, LeadingTrivia: []string{"", "", " Final comment"}, AfterNewline: true},
-	})
+	// Final comment`))
+	for i, expectedTok := range expectedToks {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+		} else if tok.Literal != expectedTok.Literal {
+			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+		} else if expectedTok.LeadingTrivia != nil && len(tok.LeadingTrivia) != len(expectedTok.LeadingTrivia) {
+			t.Errorf("token %d: expected %d leading trivia lines, got %d", i, len(expectedTok.LeadingTrivia), len(tok.LeadingTrivia))
+		} else {
+			for j, line := range expectedTok.LeadingTrivia {
+				if tok.LeadingTrivia[j] != line {
+					t.Errorf("token %d: expected %q leading trivia line, got %q", i, line, tok.LeadingTrivia[j])
+				}
+			}
+		}
+	}
 }
 
 func TestScanContinuesAfterNullCharacter(t *testing.T) {
-	expectTokenSequence(t, "Hello\x00World", []token.Token{
+	expectedToks := []token.Token{
 		{Type: token.IDENT, Literal: "Hello"},
 		{Type: token.UNKNOWN, Literal: "\x00"},
 		{Type: token.IDENT, Literal: "World"},
 		{Type: token.EOF},
-	})
+	}
+	l := New(strings.NewReader("Hello\x00World"))
+	for i, expectedTok := range expectedToks {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+		} else if tok.Literal != expectedTok.Literal {
+			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+		}
+	}
 }
 
 func TestPunctuators(t *testing.T) {
-	expectTokenSequence(t, "; = == ! != < <= > >= () {} /", []token.Token{
+	expectedToks := []token.Token{
 		{Type: token.SEMICOLON, Literal: ";"},
 		{Type: token.ASSIGN, Literal: "="},
 		{Type: token.EQ, Literal: "=="},
@@ -108,57 +157,111 @@ func TestPunctuators(t *testing.T) {
 		{Type: token.RBRACE, Literal: "}"},
 		{Type: token.DIVIDE, Literal: "/"},
 		{Type: token.EOF},
-	})
+	}
+	l := New(strings.NewReader("; = == ! != < <= > >= () {} /"))
+	for i, expectedTok := range expectedToks {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+		} else if tok.Literal != expectedTok.Literal {
+			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+		}
+	}
 }
 
 func TestSkipWhitespaces(t *testing.T) {
-	expectTokenSequence(t, "  one\ntwo\rthree\tfour \r\n five ", []token.Token{
+	expectedToks := []token.Token{
 		{Type: token.IDENT, Literal: "one"},
-		{Type: token.IDENT, Literal: "two", AfterNewline: true},
+		{Type: token.IDENT, Literal: "two"},
 		{Type: token.IDENT, Literal: "three"},
 		{Type: token.IDENT, Literal: "four"},
-		{Type: token.IDENT, Literal: "five", AfterNewline: true},
+		{Type: token.IDENT, Literal: "five"},
 		{Type: token.EOF},
-	})
+	}
+	l := New(strings.NewReader("  one\ntwo\rthree\tfour \r\n five "))
+	for i, expectedTok := range expectedToks {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+		} else if tok.Literal != expectedTok.Literal {
+			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+		}
+	}
 }
 
-func TestReadIden(t *testing.T) {
-	expectTokenSequence(t, " hello  hello123   _hello123 ", []token.Token{
+func TestReadIdent(t *testing.T) {
+	expectedToks := []token.Token{
 		{Type: token.IDENT, Literal: "hello"},
 		{Type: token.IDENT, Literal: "hello123"},
 		{Type: token.IDENT, Literal: "_hello123"},
 		{Type: token.EOF},
-	})
+	}
+	l := New(strings.NewReader(" hello  hello123   _hello123 "))
+	for i, expectedTok := range expectedToks {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+		} else if tok.Literal != expectedTok.Literal {
+			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+		}
+	}
 }
 
 func TestReadNumber(t *testing.T) {
-	expectTokenSequence(t, "123", []token.Token{
+	expectedToks := []token.Token{
 		{Type: token.NUMBER, Literal: "123"},
 		{Type: token.EOF},
-	})
+	}
+	l := New(strings.NewReader("123"))
+	for i, expectedTok := range expectedToks {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+		} else if tok.Literal != expectedTok.Literal {
+			t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+		}
+	}
 }
 
 func TestReadString(t *testing.T) {
 	t.Run("legal string", func(t *testing.T) {
-		expectTokenSequence(t, " 'Hello, World!' \"Hello, World!\"", []token.Token{
+		expectedToks := []token.Token{
 			{Type: token.STRING, Literal: "'Hello, World!'"},
 			{Type: token.STRING, Literal: "\"Hello, World!\""},
 			{Type: token.EOF},
-		})
+		}
+		l := New(strings.NewReader(" 'Hello, World!' \"Hello, World!\""))
+		for i, expectedTok := range expectedToks {
+			tok := l.NextToken()
+			if tok.Type != expectedTok.Type {
+				t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+			} else if tok.Literal != expectedTok.Literal {
+				t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+			}
+		}
 	})
 	t.Run("illegal string", func(t *testing.T) {
-		items := []string{
+		inputs := []string{
 			"'Hello, World",  // missing '
 			"'",              // missing '
 			"\"Hello, World", // missing "
 			"\"",             // missing "
 		}
-		expectTokenSequence(t, strings.Join(items, "\n"), []token.Token{
-			{Type: token.ILLEGAL, Literal: "'Hello, World", AfterNewline: false},
-			{Type: token.ILLEGAL, Literal: "'", AfterNewline: true},
-			{Type: token.ILLEGAL, Literal: "\"Hello, World", AfterNewline: true},
-			{Type: token.ILLEGAL, Literal: "\"", AfterNewline: true},
+		expectedToks := []token.Token{
+			{Type: token.ILLEGAL, Literal: "'Hello, World"},
+			{Type: token.ILLEGAL, Literal: "'"},
+			{Type: token.ILLEGAL, Literal: "\"Hello, World"},
+			{Type: token.ILLEGAL, Literal: "\""},
 			{Type: token.EOF},
-		})
+		}
+		l := New(strings.NewReader(strings.Join(inputs, "\n")))
+		for i, expectedTok := range expectedToks {
+			tok := l.NextToken()
+			if tok.Type != expectedTok.Type {
+				t.Errorf("token %d: expected type %v, got %v", i, expectedTok.Type, tok.Type)
+			} else if tok.Literal != expectedTok.Literal {
+				t.Errorf("token %d: expected %q, got %q", i, expectedTok.Literal, tok.Literal)
+			}
+		}
 	})
 }
