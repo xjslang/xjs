b4c2e8fb213be376f09ed55bde2c71b0760d4256
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-04-19 08:50:44 +0100

    refactor(parser): restructure files

--- parser/parser.go
+++ parser/parser.go
@@ -62,3 +62,77 @@ func (p *Parser) ExpectASI() error {
 func (p *Parser) ParseProgram() *ast.BlockStatement {
 	return p.parseBody()
 }
+
+func (p *Parser) ParseExpression() ast.Expression {
+	switch p.CurrentToken.Type {
+	case token.NUMBER:
+		val := p.CurrentToken.Literal
+		p.AdvanceToken()
+		return &ast.IntegerLiteral{Value: val}
+	case token.STRING:
+		val := p.CurrentToken.Literal
+		p.AdvanceToken()
+		return &ast.StringLiteral{Value: val}
+	}
+	return nil
+}
+
+func (p *Parser) parseBody() *ast.BlockStatement {
+	bodyStmt := &ast.BlockStatement{}
+	for stmt, err := p.parseStatement(); err == nil; stmt, err = p.parseStatement() {
+		bodyStmt.Statements = append(bodyStmt.Statements, stmt)
+	}
+	return bodyStmt
+}
+
+func (p *Parser) parseStatement() (ast.Statement, error) {
+	switch p.CurrentToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	case token.FUNCTION:
+		return p.parseFunction()
+	}
+	return nil, errors.New("Unexpected token " + p.CurrentToken.Literal)
+}
+
+func (p *Parser) parseLetStatement() (*ast.LetStatement, error) {
+	stmt := &ast.LetStatement{}
+	p.AdvanceToken() // consume token.LET
+	ident, err := p.Expect(token.IDENT)
+	if err != nil {
+		return stmt, err
+	}
+	stmt.Name = &ast.Identifier{Value: ident.Literal}
+	if _, err := p.Expect(token.ASSIGN); err != nil {
+		return stmt, err
+	}
+	stmt.Value = p.ParseExpression()
+	if err := p.ExpectASI(); err != nil {
+		return stmt, err
+	}
+	return stmt, nil
+}
+
+func (p *Parser) parseFunction() (*ast.FunctionDeclaration, error) {
+	stmt := &ast.FunctionDeclaration{}
+	p.AdvanceToken() // consume token.FUNCTION
+	ident, err := p.Expect(token.IDENT)
+	if err != nil {
+		return stmt, err
+	}
+	stmt.Name = &ast.Identifier{Value: ident.Literal}
+	if _, err := p.Expect(token.LPAREN); err != nil {
+		return stmt, err
+	}
+	if _, err := p.Expect(token.RPAREN); err != nil {
+		return stmt, err
+	}
+	if _, err := p.Expect(token.LBRACE); err != nil {
+		return stmt, err
+	}
+	stmt.Body = p.parseBody()
+	if _, err := p.Expect(token.RBRACE); err != nil {
+		return stmt, err
+	}
+	return stmt, nil
+}
