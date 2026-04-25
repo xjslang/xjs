586b40e7196ded1e4e2b0800fd87717277f129af
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-04-25 22:41:15 +0100

    feat(parser): advance to statement end when and error happens and keep parsing

--- parser/middleware.go
+++ parser/middleware.go
@@ -5,24 +5,24 @@ import (
 	"github.com/xjslang/xjs/token"
 )
 
-func (p *Parser) UseStatementParser(parser func(p *Parser, next func() ast.Statement) ast.Statement) {
+func (p *Parser) UseStatementParser(parser func(p *Parser, next func() (ast.Statement, error)) (ast.Statement, error)) {
 	next := p.statementParser
 	if next == nil {
 		next = defaultStatementParser
 	}
-	p.statementParser = func(p *Parser) ast.Statement {
-		return parser(p, func() ast.Statement {
+	p.statementParser = func(p *Parser) (ast.Statement, error) {
+		return parser(p, func() (ast.Statement, error) {
 			return next(p)
 		})
 	}
 }
 
-func defaultStatementParser(p *Parser) ast.Statement {
+func defaultStatementParser(p *Parser) (ast.Statement, error) {
 	switch p.CurrentToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
 	case token.FUNCTION:
 		return p.parseFunction()
 	}
-	return nil
+	return nil, nil
 }
