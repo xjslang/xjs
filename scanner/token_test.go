58a8606f537c2ca6245846e3ddf0073a7c56e528
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-05-12 10:55:05 +0100

    refactor(test): rename `package` with `package_test` in test files

--- scanner/token_test.go
+++ scanner/token_test.go
@@ -1,40 +1,31 @@
-package scanner
+package scanner_test
 
 import (
 	"sync"
 	"testing"
+
+	"github.com/xjslang/xjs/scanner"
 )
 
-func TestRegisterKind(t *testing.T) {
-	lit := "**"
-	powType := RegisterKind(lit)
-	if powType < initCustomType {
-		t.Errorf("Expected type to be greater than %d, got %d", initCustomType, powType)
+func TestConcurrentKindAccess(t *testing.T) {
+	n := 100
+	types := make([]scanner.Kind, n)
+	var wg sync.WaitGroup
+	for i := range n {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			types[i] = scanner.RegisterKind("aaa")
+		}(i)
 	}
-	if powType.String() != lit {
-		t.Errorf("Expected %q, got %q", lit, powType.String())
+	wg.Wait()
+
+	// check that types has no duplicates
+	seen := make(map[scanner.Kind]bool)
+	for _, typ := range types {
+		if seen[typ] {
+			t.Fatalf("Duplicate!")
+		}
+		seen[typ] = true
 	}
-
-	t.Run("concurrent access returns unique keys", func(t *testing.T) {
-		n := 100
-		types := make([]Kind, n)
-		var wg sync.WaitGroup
-		for i := range n {
-			wg.Add(1)
-			go func(i int) {
-				defer wg.Done()
-				types[i] = RegisterKind("aaa")
-			}(i)
-		}
-		wg.Wait()
-
-		// check that types has no duplicates
-		seen := make(map[Kind]bool)
-		for _, typ := range types {
-			if seen[typ] {
-				t.Fatalf("Duplicate!")
-			}
-			seen[typ] = true
-		}
-	})
 }
