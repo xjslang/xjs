b9203cb3083a1a93600b4120cbe05f9eea7631ac
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-05-19 22:10:18 +0100

    docs(example): add basic usage example

---
+++ examples/djs/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xjslang/xjs/ast"
+	"github.com/xjslang/xjs/parser"
+	"github.com/xjslang/xjs/printer"
+	"github.com/xjslang/xjs/scanner"
+	"github.com/xjslang/xjs/token"
+)
+
+var deferTyp = token.RegisterType("defer")
+
+// implements ast.Node
+type DeferStmt struct {
+	DeferToken token.Token
+	Stmt       *ast.ExprStmt
+}
+
+func (node *DeferStmt) Type() string {
+	return "DeferStmt"
+}
+
+func main() {
+	input := `
+	function foo() {
+		let db = openDb()
+		// ensures closing db properly
+		defer closeDb()
+	}`
+
+	// create a scanner that can read "defer"
+	djsScanner := &scanner.Scanner{}
+	djsScanner.UseScanner(func(sc *scanner.Scanner, next func() token.Token) token.Token {
+		tok := next()
+		if tok.Type == token.IDENT && tok.Literal == "defer" {
+			tok.Type = deferTyp
+		}
+		return tok
+	})
+	djsScanner.Init([]byte(input))
+
+	// create a parser that can parse "defer"
+	djsParser := &parser.Parser{}
+	djsParser.UseStmtParser(func(p *parser.Parser, next func() (ast.Node, error)) (node ast.Node, err error) {
+		if p.CurrentToken.Type == deferTyp {
+			deferStmt := &DeferStmt{DeferToken: p.CurrentToken}
+			p.AdvanceToken() // consume "defer"
+			if deferStmt.Stmt, err = parser.ParseExprStmt(p); err != nil {
+				return
+			}
+			node = deferStmt
+			return
+		}
+		return next()
+	})
+	djsParser.Init(djsScanner)
+	node, err := parser.ParseProgram(djsParser)
+	if err != nil {
+		panic(err)
+	}
+
+	// create a compiler that can compile `DeferStmt`
+	djsCompiler := &printer.Printer{}
+	djsCompiler.UsePrinter(func(pr *printer.Printer, node ast.Node, next func()) {
+		if node, ok := node.(*DeferStmt); ok {
+			pr.PrintTrivia(node.DeferToken.LeadingTrivia) // print previous comments and new lines
+			pr.EnsureLine()                               // ensure a new line is added before printing
+			pr.PrintIndentedString("{using _ = {[Symbol.dispose]() {")
+			pr.PrintNode(node.Stmt.Expr)
+			pr.PrintToken(node.Stmt.SemiToken)
+			pr.PrintIndentedString("}}}")
+			return
+		}
+		next()
+	})
+	djsCompiler.Init()
+	djsCompiler.PrintNode(node)
+	fmt.Println(djsCompiler.String())
+
+	// create a formatter that can format `DeferStmt`
+	djsFormatter := &printer.Printer{}
+	djsFormatter.UsePrinter(func(pr *printer.Printer, node ast.Node, next func()) {
+		if node, ok := node.(*DeferStmt); ok {
+			pr.EnsureLine() // ensure a new line is added before printing
+			pr.PrintToken(node.DeferToken)
+			pr.EnsureSpace() // ensure a new space is added before printing
+			pr.PrintNode(node.Stmt.Expr)
+			pr.PrintToken(node.Stmt.SemiToken)
+			return
+		}
+		next()
+	})
+	djsFormatter.Init()
+	djsFormatter.PrintNode(node)
+	fmt.Println(djsFormatter.String())
+}
