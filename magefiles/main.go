f2ff332c904e88c2a130d97348e7720c6efbf0cb
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-06-01 12:37:12 +0100

    chore(mage): rename updateGoldenFiles command by update

--- magefiles/main.go
+++ magefiles/main.go
@@ -30,12 +30,6 @@ func Bench() error {
 	return sh.RunV("go", "test", "./...", "-bench=.", "-benchtime=3s", "-run=^$")
 }
 
-func UpdateGoldenFiles() (err error) {
-	if err = sh.RunV("go", "test", "./xjs_test.go", "-run", "^TestGoldenFiles$", "-update"); err != nil {
-		return
-	}
-	if err = sh.RunV("go", "test", "./printer", "-update"); err != nil {
-		return
-	}
-	return
+func Update() error {
+	return sh.RunV("go", "test", "./printer", "-update")
 }
