379701035e4e975700a66a4eb2e0f18c46b47f7b
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-06-04 23:23:35 +0100

    feat(parser): parse continue statement
    and  move `testdata/break_stmt.js` examples to `testdata/for_stmt.js`.

--- testdata/for_stmt.js
+++ testdata/for_stmt.js
@@ -24,3 +24,20 @@ outer: for (let i = 0; i < 5; i = i + 1) {
 }
 
 label: print("statement is required after a label");
+
+for (let i = 0; i < 10; i = i + 1) {
+  if (i == 3) {
+    continue;
+  }
+  text = text + i;
+}
+
+// The first for statement is labeled "loop1"
+loop1: for (let i = 0; i < 3; i = i + 1) {
+  // The second for statement is labeled "loop2"
+  loop2: for (let j = 0; j < 3; j = j + 1) {
+    if (i == 1 && j == 1) {
+      continue loop1;
+    }
+  }
+}
