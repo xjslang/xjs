0c5c7eef342586610ff59dc4fc4d4169c350ad71
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-06-04 23:24:34 +0100

    refactor(test): reorganize js tests

--- testdata/for_stmt.js
+++ testdata/for_stmt.js
@@ -10,3 +10,17 @@ for (
 ) {
   print(i);
 }
+
+for (let i = 0; i < 10; i = i + 1) {
+  break;
+}
+
+outer: for (let i = 0; i < 5; i = i + 1) {
+  for (let j = 0; j < 5; j = j + 1) {
+    if (i == 2 && j == 3) {
+      break outer; // Exits the entire outer loop
+    }
+  }
+}
+
+label: print("statement is required after a label");
