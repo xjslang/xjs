f8448f96f87051d4ae6a73da855701b28bc836b0
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-06-21 18:55:58 +0100

    refactor(printer): update parameter naming in UsePrinter method

--- printer/middleware.go
+++ printer/middleware.go
@@ -4,12 +4,12 @@ import (
 	"github.com/xjslang/xjs/ast"
 )
 
-func (p *Printer) UsePrinter(printer func(p *Printer, node ast.Node, next func(node ast.Node) error) error) {
-	print := p.printer
-	if p.printer == nil {
+func (pr *Printer) UsePrinter(printer func(pr *Printer, node ast.Node, next func(node ast.Node) error) error) {
+	print := pr.printer
+	if pr.printer == nil {
 		print = defaultPrinter
 	}
-	p.printer = func(p *Printer, node ast.Node) error {
+	pr.printer = func(p *Printer, node ast.Node) error {
 		return printer(p, node, func(node ast.Node) error {
 			return print(p, node)
 		})
