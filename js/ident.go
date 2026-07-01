58638ff918ce8384a49059608cb5247d0ddfa984
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-07-01 10:58:57 +0100

    refactor(parser): reorganize code

--- js/ident.go
+++ js/ident.go
@@ -1,12 +1,9 @@
 package js
 
 import (
-	"unicode/utf8"
-
 	"github.com/xjslang/xjs/ast"
 	"github.com/xjslang/xjs/parser"
 	"github.com/xjslang/xjs/printer"
-	"github.com/xjslang/xjs/scanner"
 	"github.com/xjslang/xjs/token"
 )
 
@@ -21,23 +18,6 @@ func ParseIdent(p *parser.Parser) (node *Ident, err error) {
 		return
 	}
 	return node, nil
-}
-
-func ParseMemberKey(p *parser.Parser) (node *Ident, err error) {
-	node = &Ident{}
-	if node.Token, err = ExpectMemberKey(p); err != nil {
-		return
-	}
-	return node, nil
-}
-
-func ExpectMemberKey(p *parser.Parser) (token.Token, error) {
-	tok := p.CurrentToken
-	if r, s := utf8.DecodeRuneInString(tok.Literal); s == 0 || !scanner.IsLetter(r) {
-		return tok, p.Error("key expected")
-	}
-	p.AdvanceToken()
-	return tok, nil
 }
 
 func PrintIdent(p *printer.Printer, node *Ident) {
