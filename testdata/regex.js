983debb67a631016babecab7c5244827435ceb76
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-07-28 22:00:24 +0100

    feat(parser,jsextended): implement regular expressions

---
+++ testdata/regex.js
@@ -0,0 +1,5 @@
+let email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+let x = /abc/gi + "blah";
+let hasAbc = /abc/gi.test("abc");
+let slashInClass = /[/]/.test("/");
+let inArray = [/a/, /b/][0];
