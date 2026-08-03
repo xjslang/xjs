458a1af944860227b01f6bc94c7eca5a4df3d258
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-08-03 19:01:36 +0100

    fix(parse,js): allow decimals to start with '.'

--- js/testdata/roundtrip/numbers.js
+++ js/testdata/roundtrip/numbers.js
@@ -1,27 +1,40 @@
 // hexadecimal numbers
-let a = 0x10;
-let b = 0X20;
-let c = 0xABCDEF;
-let d = 0xabcdef;
-let e = 0x123ABC;
-let f = 0xFFFFFF;
-let g = 0x0;
-let h = 0xF;
+0x10;
+0X20;
+0xABCDEF;
+0xabcdef;
+0x123ABC;
+0xFFFFFF;
+0x0;
+0xF;
 
 // octal numbers
-let a = 0o10;
-let b = 0O20;
-let c = 0o1234567;
-let d = 0o0;
-let e = 0o7;
-let f = 0o777;
-let g = 0o0012;
-let h = 0O01234567;
+0o10;
+0O20;
+0o1234567;
+0o0;
+0o7;
+0o777;
+0o0012;
+0O01234567;
 
 // binary numbers
-let a = 0b0;
-let b = 0B0;
-let c = 0b1;
-let c = 0b10;
-let c = 0b0101;
-let h = 0B00001;
+0b0;
+0B0;
+0b1;
+0b10;
+0b0101;
+0B00001;
+
+// integers
+0;
+1;
+123;
+456;
+
+// decimals
+0.123;
+.123;
+.1;
+0.123e+12;
+.123e+12;
