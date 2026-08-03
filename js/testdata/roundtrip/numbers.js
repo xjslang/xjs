5733208c3712933d4eaaf18df2ee725e132e23ab
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-08-03 17:25:49 +0100

    feat(parser,js): parse binary numbers

---
+++ js/testdata/roundtrip/numbers.js
@@ -0,0 +1,27 @@
+// hexadecimal numbers
+let a = 0x10;
+let b = 0X20;
+let c = 0xABCDEF;
+let d = 0xabcdef;
+let e = 0x123ABC;
+let f = 0xFFFFFF;
+let g = 0x0;
+let h = 0xF;
+
+// octal numbers
+let a = 0o10;
+let b = 0O20;
+let c = 0o1234567;
+let d = 0o0;
+let e = 0o7;
+let f = 0o777;
+let g = 0o0012;
+let h = 0O01234567;
+
+// binary numbers
+let a = 0b0;
+let b = 0B0;
+let c = 0b1;
+let c = 0b10;
+let c = 0b0101;
+let h = 0B00001;
